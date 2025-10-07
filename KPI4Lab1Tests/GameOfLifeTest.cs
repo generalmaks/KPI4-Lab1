4574a11bc7009c3afb8708fd637de5ca74809d7b
generalmaks <schrinan@gmail.com>
2025-10-07 19:03:21 +0300

    added more tests

--- KPI4Lab1Tests/GameOfLifeTest.cs
+++ KPI4Lab1Tests/GameOfLifeTest.cs
@@ -34,8 +34,47 @@ public class Tests
             { false, false, false, false, false },
             { false, false, false, false, false }
         };
-        _gameOfLife = new GameOfLife(_rows, _columns, initState);
+        _gameOfLife = new GameOfLife(initState);
         _gameOfLife.Step();
         Assert.That(_gameOfLife.Grid, Is.EqualTo(secondState));
+    }
+    
+    [Test]
+    public void Step_ZeroGrid_ReturnsZeroGrid()
+    {
+        var initState = new bool[,]
+            { };
+        _gameOfLife = new GameOfLife(initState);
+
+        _gameOfLife.Step();
+        Assert.That(_gameOfLife.Grid, Is.EqualTo(initState));
+    }
+
+    [Test]
+    public void Step_InputTub_ReturnsTub()
+    {
+        var initState = new bool[,]
+        {
+            { false, true, false },
+            { true, false, true },
+            { false, true, false },
+        };
+        _gameOfLife = new GameOfLife(initState);
+        _gameOfLife.Step();
+        Assert.That(_gameOfLife.Grid, Is.EqualTo(initState));
+    }
+
+    [Test]
+    public void Step_InputBeehive()
+    {
+        var initState = new bool[,]
+        {
+            { false, true, true, false },
+            { false, false, false, true },
+            { false, true, true, false },
+        };
+        _gameOfLife = new GameOfLife(initState);
+        _gameOfLife.Step();
+        Assert.That(_gameOfLife.Grid, Is.EqualTo(initState));
     }
 }
