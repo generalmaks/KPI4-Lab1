a972980d0b8eafed6777b1403cbb1ca8bc93eef1
generalmaks <schrinan@gmail.com>
2025-10-08 19:11:57 +0300

    Added tests for FileParser

---
+++ KPI4Lab1Tests/FileParserTest.cs
@@ -0,0 +1,73 @@
+﻿using KPI4Lab1;
+
+namespace KPI4Lab1Tests;
+
+public class FileParserTest
+{
+    [Test]
+    public void Parse_CorrectInput_CorrectOutput()
+    {
+        const string input = """
+                             3
+                             3 3
+                             .x.
+                             xxx
+                             xx.
+                             """;
+        var expectedGrid = new bool[,]
+        {
+            { false, true, false },
+            { true, true, true },
+            { true, true, false }
+        };
+        
+        var (generations, grid) = FileParser.Parse(input.Split('\n'));
+        Assert.Multiple(() =>
+        {
+            Assert.That(generations, Is.EqualTo(3));
+            Assert.That(grid, Is.EqualTo(expectedGrid));
+        });
+    }
+
+    [Test]
+    public void Parse_IncorrectSizeInput_ThrowsException()
+    {
+        const string input = """
+                             3
+                             2 5
+                             .x.
+                             xxx
+                             xx.
+                             """;
+        Assert.That(() => FileParser.Parse(input.Split('\n')),
+            Throws.TypeOf<ArgumentException>());
+    }
+
+    [Test]
+    public void Parse_DifferentRowSizes_ThrowsException()
+    {
+        const string input = """
+                             3
+                             3 3
+                             .x..
+                             xxx
+                             xx.
+                             """;
+        Assert.That(() => FileParser.Parse(input.Split('\n')),
+            Throws.TypeOf<ArgumentException>());
+    }
+
+    [Test]
+    public void Parse_ZeroOrNegativeValues_ThrowsException()
+    {
+        const string input = """
+                             -3
+                             3 3
+                             .x.
+                             xxx
+                             xx.
+                             """;
+        Assert.That(() => FileParser.Parse(input.Split('\n')),
+            Throws.TypeOf<ArgumentException>());
+    }
+}
